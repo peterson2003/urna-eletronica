45cb0cdc557705027ea56ae3ce23724693bbb4bb
Peterson Santos <petersonkfs@outlook.com>
2022-09-02 19:14:26 -0300

    urna eletronica

---
+++ urna/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '22222',
+                nome: 'André Ferreira',
+                partido: 'PL',
+                fotos:[
+                    {url:'andreferreira.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Daniel Coelho',
+                partido: 'PSDB',
+                fotos:[
+                    {url:'danielcoelho.png', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '40',
+                nome: 'João Campos',
+                partido: 'PSB',
+                vice: 'Isabella de Roldão',
+                fotos:[
+                    {url:'joaocampos.jpg', legenda: 'Prefeito'},
+                    {url:'isabelladeroldao.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '77',
+                nome: 'Marília Arraes',
+                partido: 'Solidariedade',
+                vice: 'Sebastião Oliveira',
+                fotos:[
+                    {url:'mariliaarraes.jpg', legenda: 'Prefeito'},
+                    {url:'sebastiaooliveira.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
